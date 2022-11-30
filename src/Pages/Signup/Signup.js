4d020092e90f0f09f0c4b628798f9e85256bcd78
naimurrahmanniloy <niloyrahman432@gmail.com>
2022-11-30 15:46:15 +0600

    take the users to the backend server

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -23,12 +23,29 @@ const Signup = () => {
                     displayName: data.name
                 }
                 updateUser(userInfo)
-                    .then(() => { })
+                    .then(() => {
+                        saveUser(data.name, data.email, data.role);
+                    })
                     .catch(err => console.log(err))
             })
             .catch(error => {
                 console.log(error)
                 setSignUpError(error.message)
+            })
+    }
+
+    const saveUser = (name, email, role) => {
+        const user = { name, email, role };
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
             })
     }
 
